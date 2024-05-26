b42e80f4480e31cd4192895858e97b10f74eeefb
Eliecer Ureche <elieceru.job@gmail.com>
2024-05-25 22:35:05 -0500

    modulos creados

--- mvc-login/src/main/java/com/auth0/example/SecurityConfig.java
+++ mvc-login/src/main/java/com/auth0/example/SecurityConfig.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +24,9 @@ public class SecurityConfig {
     private String issuer;
     @Value("${okta.oauth2.client-id}")
     private String clientId;
+
+    @Value("${okta.oauth2.audience:}")
+    private String audience;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -33,6 +41,23 @@ public class SecurityConfig {
         return http.build();
     }
 
+    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(
+            ClientRegistrationRepository clientRegistrationRepository) {
+
+        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(
+                        clientRegistrationRepository, "/oauth2/authorization");
+        authorizationRequestResolver.setAuthorizationRequestCustomizer(
+                authorizationRequestCustomizer());
+
+        return authorizationRequestResolver;
+    }
+
+    private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
+        return customizer -> customizer
+                .additionalParameters(params -> params.put("audience", audience));
+    }
+
     private LogoutHandler logoutHandler() {
         return (request, response, authentication) -> {
             try {
@@ -43,4 +68,5 @@ public class SecurityConfig {
             }
         };
     }
+
 }
