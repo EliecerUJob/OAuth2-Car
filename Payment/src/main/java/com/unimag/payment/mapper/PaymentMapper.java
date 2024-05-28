11a7494d4116e0fc37ca386bb960ba43dad5eea0
Eliecer Ureche <elieceru.job@gmail.com>
2024-05-28 16:04:16 -0500

    pull

--- Payment/src/main/java/com/unimag/payment/mapper/PaymentMapper.java
+++ Payment/src/main/java/com/unimag/payment/mapper/PaymentMapper.java
@@ -4,9 +4,11 @@ import com.unimag.payment.entity.PaymentDTO;
 import com.unimag.payment.entity.PaymentDTOSave;
 import com.unimag.payment.entity.PaymentEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
     PaymentDTO toDto(PaymentEntity entity);
+    @Mapping(target = "id", ignore = true)
     PaymentEntity toEntity(PaymentDTOSave dto);
 }
